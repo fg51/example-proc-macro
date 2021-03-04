d73d143cd5d2d2ace76acf98bc4b7134a391283e
kflange <zknsftk1msrk@gmail.com>
2021-03-04 19:06:01 +0900

    Added: macro

--- hello_macro/hello_macro_derive/src/lib.rs
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -1,3 +1,34 @@
+use proc_macro::TokenStream;
+
+// The "syn" crate parses Rust code from a string into a data structure that we can perform
+// operations on.
+use syn;
+
+// The "quote" crate turns "syn" data structures back into Rust code.
+use quote::quote;
+
+// "hello_macro_derive" function will be called when a user specifies #[derive(HelloMacro] on a
+// type.
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap_or_else(|e| panic!("{}", e)); // from input to ast.
+
+    // Build the trait implementation.
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
